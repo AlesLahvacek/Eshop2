5d9396b6007a47928f1355a806ebe20ac885aafa
AlesLahvacek <hlavacal21@zaci.spse.cz>
2024-01-06 10:52:26 +0100

    priprava na uzivatele

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+router.use(express.json())
+
+const users = []
 
 router.get("/", (req,res) => {
-    res.send("User list")
+    res.json(users)
     })
     
-    
+router.post("/", (req,res) => {
+
+})    
+
 router.get("/new", (req,res) => {
     res.send("new user form")
 })
