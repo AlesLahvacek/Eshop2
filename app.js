8bd93eb292dc690fa6c0faf9977f0e352c9bd776
AlesLahvacek <hlavacal21@zaci.spse.cz>
2024-01-16 11:58:57 +0100

    objednávka

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const mysql = require('mysql');
 const Db = require('./mysqlPromise')
 const connectionString = 'mysql://root@localhost/wp3f'
 const session = require('express-session')
+app.use( express.urlencoded({ extended: true }))
 
+app.use(express.json())
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/views"))
 app.use(session({
@@ -61,7 +63,7 @@ const db = mysql.createConnection({
     res.redirect('/');
 });
  
-app.get('/kosik', function(req, res) {
+app.get('/kosik', (req, res) => {
     let produkty = req.session.kosik || [];
     
     if (produkty.length > 0) {
@@ -91,7 +93,33 @@ app.get('/kosik', function(req, res) {
     }
 });
 
-app.get('/vymazat-kosik', function(req, res) {
+app.get('/kosik/objednavka', (req, res) => {
+    res.render('objednavka');
+});
+
+app.post('/odeslat-objednavku', (req, res) => {
+    const { jmeno, prijmeni, email, telefon, adresa } = req.body;
+    
+    const produkty = JSON.stringify(req.session.kosik || []);
+    
+    if (!jmeno || !prijmeni || !email || !telefon || !adresa) {
+        return res.status(400).send('Všechna pole formuláře musí být vyplněna.');
+        
+      }
+
+    const query = 'INSERT INTO objednavka (jmeno, prijmeni, email, telefon, adresa, produkty, vyrizena) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    
+    db.query(query, [jmeno, prijmeni, email, telefon, adresa, produkty, 0], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Došlo k chybě při zpracování objednávky.');
+        } else {
+            res.send('Objednávka byla úspěšně odeslána.');
+        }
+    });
+});
+
+app.get('/vymazat-kosik', (req, res) => {
     req.session.kosik = [];
     res.redirect('/kosik');
 });
